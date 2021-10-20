16c6d0520645bf60b5d2d42bf9773e4b4d815e86
Tai all <70927364+hoanghuutai0605@users.noreply.github.com>
2021-10-20 21:41:38 +0700

    add folder NodeJsBoiler

---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+module.exports.Token = require('./token.model');
+module.exports.User = require('./user.model');
+module.exports.Class = require('./class.model');
+module.exports.Student = require('./student.model');
+module.exports.Category = require('./category.model');
+module.exports.Product = require('./product.model');
+module.exports.ApparelSize = require('./apparelSize.model');
---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+module.exports.authService = require('./auth.service');
+module.exports.emailService = require('./email.service');
+module.exports.tokenService = require('./token.service');
+module.exports.userService = require('./user.service');
+module.exports.classService = require('./class.service');
+module.exports.studentService = require('./student.service');
+module.exports.categoryService = require('./category.service');
+module.exports.productService = require('./product.service');
+module.exports.apparelSizeService = require('./apparelSize.service');
